40f164b662c67052123e0c825f75bc20ebd81c3c
step0035 <step0035@e.ntu.edu.sg>
2021-02-18 02:35:30 +0800

    edit app.py, added heading and tabs

--- app.py
+++ app.py
@@ -4,34 +4,74 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
 
+df = pd.read_csv('./data/clean_csv_data/new_bus_arrival_18051.csv')
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
+#set content of tab1
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("This is tab 1!", className="card-text"),
+            dbc.Button("Click here", color="success"),
+        ]
+    ),
+    className="mt-3",
+)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#set content of tab2
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("This is tab 2!", className="card-text"),
+            dbc.Button("Don't click here", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#set app layour
+app.layout = html.Div([
 
-app.layout = html.Div(children=[
-    html.H4(children='US Agriculture Exports (2011)'),
-    generate_table(df)
+    dbc.Container(
+        [
+            html.H1("Hello", className="display-3"),
+            html.P("Welcome to my Dashboard, my name is hululuhuhu", className="lead"),
+            html.Hr(className="my-4"),
+
+            html.Div(
+                [
+                    dbc.Tabs(
+                        [
+                            dbc.Tab(label="Tab 1", tab_id="tab-1"),
+                            dbc.Tab(label="Tab 2", tab_id="tab-2")
+                        ],
+                        id="tabs",
+                        active_tab="tab-1"
+                    ),
+                    html.Div(id="content")
+                ]
+            )
+        ],
+        className="jumbotron"
+    )
 ])
+
+@app.callback(
+    Output("content", "children"),
+    [Input("tabs", "active_tab")]
+)
+def switch_tab(at):
+    if at == "tab-1":
+        return tab1_content
+    elif at == "tab-2":
+        return tab2_content
 
 if __name__ == '__main__':
     app.run_server(debug=True)
